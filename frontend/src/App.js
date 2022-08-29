89cf18988dffc51cba31615435216df45001e1ca
Jagadeeswaran <jagan@arvension.com>
2022-08-29 08:02:04 +0530

    day2

---
+++ frontend/src/App.js
@@ -0,0 +1,155 @@
+
+function App() {
+  return (
+    <div class="gird-container">
+        <header class="row">
+            <div>
+                <a class="brand" href="index.html">amazon</a>
+            </div>
+            <div>
+                <a href="cart.html" class="cart">Cart</a>
+                <a href="signin.html">SignIn</a>
+            </div>
+
+        </header>
+        <main>
+            <div class="row center">
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/p4.jpg" alt="product"/>
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2> Slim Shirt </h2>
+                        </a>
+                        <div class="rating">
+                            {/* <!-- Star --> */}
+                            <span> <i class="fa fa-star"></i></span>
+                            <span><i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                        </div>
+                        {/* <!-- Price section --> */}
+                        <div class="price">
+                            Rs 550
+
+                        </div>
+                    </div>
+                    
+                </div>
+                {/* <!-- 2 --> */}
+                <div class="card">
+                    <a href="product.html">
+                        {/* <!--Imagesize:680px by 830px--> */}
+                        <img class="medium" src="./images/p5.jpg" alt="product"/>
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2> Slim Shirt </h2>
+                        </a>
+                        <div class="rating">
+                            {/* <!-- Star --> */}
+                            <span> <i class="fa fa-star"></i></span>
+                            <span><i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                        </div>
+                        {/* <!-- Price section --> */}
+                        <div class="price">
+                            Rs 550
+
+                        </div>
+                    </div>
+                    
+                </div>
+                {/* <!-- 3 --> */}
+                <div class="card">
+                    <a href="product.html">
+                        {/* <!--Imagesize:680px by 830px--> */}
+                        <img class="medium" src="./images/p6.jpg" alt="product"/>
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2> Slim Shirt </h2>
+                        </a>
+                        <div class="rating">
+                            {/* <!-- Star --> */}
+                            <span> <i class="fa fa-star"></i></span>
+                            <span><i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                        </div>
+                        {/* <!-- Price section --> */}
+                        <div class="price">
+                            Rs 550
+
+                        </div>
+                    </div>
+                    
+                </div>
+                {/* <!-- 4 --> */}
+                <div class="card">
+                    <a href="product.html">
+                        {/* <!--Imagesize:680px by 830px--> */}
+                        <img class="medium" src="./images/p4.jpg" alt="product"/>
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2> Slim Shirt </h2>
+                        </a>
+                        <div class="rating">
+                            {/* <!-- Star --> */}
+                            <span> <i class="fa fa-star"></i></span>
+                            <span><i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star-o"></i></span>
+                        </div>
+                        {/* <!-- Price section --> */}
+                        <div class="price">
+                            Rs 550
+
+                        </div>
+                    </div>
+                    
+                </div>
+                {/* <!-- 5 --> */}
+                <div class="card">
+                    <a href="product.html">
+                        {/* <!--Imagesize:680px by 830px--> */}
+                        <img class="medium" src="./images/p4.jpg" alt="product"/>
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2> Slim Shirt </h2>
+                        </a>
+                        <div class="rating">
+                            {/* <!-- Star --> */}
+                            <span> <i class="fa fa-star"></i></span>
+                            <span><i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star"></i></span>
+                            <span> <i class="fa fa-star-half-0"></i></span>
+                        </div>
+                        {/* price */}
+                        <div class="price">
+                            Rs 550
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </main>
+        <footer class="row center">
+            All right reserved
+        </footer>
+    </div>
+  );
+}
+
+export default App;
